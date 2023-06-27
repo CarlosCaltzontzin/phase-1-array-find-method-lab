233128db90dc69acceadd51bb1391843e5a07f6a
Carlos Caltzontzin <cacr71993@yahoo.com>
2023-06-27 01:04:14 -0500

    completed phase-1-array-find-method-lad

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const denverBroncosWinYear = superbowlWin 
+function superbowlWin(record){
+    //const win = arr.find(obj => obj.result === 'W' && obj.team === 'Denver Broncos');
+    //return win 
+
+    const data = record.find(record => record.result === 'W')
+    if(data){
+        return data.year;
+    }
+    else{
+    return data
+    }
+} 
